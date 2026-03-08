d82a685712d02ce4fa862fd9c7293f6180e355a0
Roman Kryvko <roman.kr43@gmail.com>
2026-03-09 00:48:52 +0100

    Implement splitting lines by words.

--- catdate/bot.py
+++ catdate/bot.py
@@ -58,23 +58,54 @@ def draw_outlined_text(draw: ImageDraw.ImageDraw, xy: tuple[float, float], text:
     draw.text(xy, text, fill=main, font=font, anchor=anchor, align=align)
 
 
-#FIXME: preserve words
-def split_lines(text: str, textlength: int, maxlength: int) -> str:
-    if textlength < maxlength:
-        return text
-
-    ratio = textlength / maxlength
-
+def split_lines(text: str, char_to_px: float) -> str:
     parts = []
     start = 0
-    for i in range(1, math.ceil(ratio)):
-        idx = math.floor(i * len(text) / ratio)
+    for i in range(1, math.ceil(char_to_px)):
+        idx = math.floor(i * len(text) / char_to_px)
         parts.append(text[start:idx])
         start = idx
 
     parts.append(text[start:])
     return "\n".join(parts)
 
+def split_line_by_words(text: str, textlength: int, maxlength: int) -> str:
+    if textlength < maxlength:
+        return text
+
+    char_to_px = textlength / maxlength
+    prev = 0
+    parts: list[str] = []
+
+    while prev < len(text):
+        start_idx = prev + math.floor((len(text) - prev) / char_to_px)
+
+        if start_idx >= len(text):
+            parts.append(text[prev:])
+            break
+
+        if text[start_idx] == " ":
+            parts.append(text[prev:start_idx])
+            prev = start_idx + 1
+            continue
+
+        space_left = text.rfind(" ", prev, start_idx)
+
+        if space_left != -1:
+            parts.append(text[prev:space_left])
+            prev = space_left + 1
+            continue
+
+        space_right = text.find(" ", start_idx)
+
+        if space_right == -1:
+            parts.append(text[prev:])
+            break
+
+        parts.append(text[prev:space_right])
+        prev = space_right + 1
+
+    return "\n".join(parts)
 
 def put_text_over_image() -> bytes:
     today = datetime.now()
--- tests/string-util_test.py
+++ tests/string-util_test.py
@@ -1,19 +1,21 @@
 import unittest
-from catdate.bot import split_lines
+from catdate.bot import split_line_by_words, split_lines
 
 class TestStringUtil(unittest.TestCase):
     def test_splits_lines(self):
         data = "1234567890"
         expected = "1234\n5678\n90"
-        actual = split_lines(data, 100, 40)
-        self.assertEqual(actual, expected)
-
-        expected = "1234\n5678\n90"
-        actual = split_lines(data, 1000, 400)
+        actual = split_lines(data, 100/40)
         self.assertEqual(actual, expected)
 
         expected = "123456\n7890"
-        actual = split_lines(data, 100, 60)
+        actual = split_lines(data, 100/60)
+        self.assertEqual(actual, expected)
+
+    def test_splitting_preserves_words(self):
+        data = "word1 word2 a-very-long-word-1111 words"
+        expected  = "word1 word2\na-very-long-word-1111\nwords"
+        actual = split_line_by_words(data, 100, 40)
         self.assertEqual(actual, expected)
 
 
